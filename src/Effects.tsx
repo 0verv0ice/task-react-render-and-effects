6e867210fed0a65e9f31c916d6d4c46245921c20
0verv0ice <theznoy@gmail.com>
2022-05-31 19:50:42 +0500

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, newmessageset] = useState(-1);
+
+    useEffect(() => {
+        const a = (newMessage: number) => newmessageset(newMessage);
+        subscribe(props.sourceId, a);
+        return () => {
+            unsubscribe(props.sourceId, a);
+            newmessageset(-1);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {' '}
+            {props.sourceId}: {message}{' '}
+        </div>
+    );
 }
